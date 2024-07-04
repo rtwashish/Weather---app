706a9f513aa2808e403efbc53129475eaaf1d957
rtwashish <144202595+rtwashish@users.noreply.github.com>
2024-07-04 11:21:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '52c91023famshb4c1f8ca95e452cp13aef7jsnd298518df5fa',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
